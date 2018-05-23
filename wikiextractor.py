abeb42d3fcc1e4bff810a49dfe339baee09a8911
Joschua Fink <joschua.fink@gmx.de>
2018-05-23 13:47:57 +0200

    remove special characters from words

--- wikiextractor.py
+++ wikiextractor.py
@@ -5,11 +5,19 @@ def unique_list(l):
     [ulist.append(x) for x in l if x not in ulist]
     return ulist
 
+def remove_specials(string):
+    return ''.join(c for c in string if c.isalnum())
+    
+
 p = wikipedia.page("Sterling Archer")
 print(p.url)
 print(p.title)
 content = p.content
 wordlist = unique_list(content.split(" "))
+
+for x in range(0, len(wordlist)):
+    wordlist[x] = remove_specials(wordlist[x])
+
 content = "\n".join(wordlist)
 
 file = open("wordlist.txt", "w")

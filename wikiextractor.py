9226b3be27fea8049d8f1b611f9532454c4a0cf3
Joschua Fink <joschua.fink@gmx.de>
2018-05-25 12:15:08 +0200

    added comments n stuff

--- wikiextractor.py
+++ wikiextractor.py
@@ -8,18 +8,41 @@ def unique_list(l):
 def remove_specials(string):
     return ''.join(c for c in string if c.isalnum())
     
+# set the language of wikipedia
+wikipedia.set_lang('de')
 
-p = wikipedia.page("Sterling Archer")
+# present options if page was not found or disambiguation page
+try:
+    p = wikipedia.page("Archer (Zeichentrickserie)")
+except wikipedia.exceptions.DisambiguationError as e:
+    print e.options
+
+# print the url and title of the article
 print(p.url)
 print(p.title)
-content = p.content
+content = p.content # pull the content of the page
+
+# few replacements
+content = content.replace('-', ' ')
+content = content.replace('\n', ' ')
+# whole string to lower case
+content = content.lower()
+
+# save words in a wordlist and remove duplicates
 wordlist = unique_list(content.split(" "))
 
-for x in range(0, len(wordlist)):
-    wordlist[x] = remove_specials(wordlist[x])
 
-content = "\n".join(wordlist)
+wlist = []
+# remove special characters from every word in wordlist
+for i in range(0, len(wordlist)):
+    word = wordlist[i]
+    if word != '' and word != ' ' and word != '\n':
+        wlist.append(remove_specials(word))
 
+# join the list of words to a single string, with each word seperate by newline
+content = "\n".join(wlist)
+
+# write the wordlist to a file
 file = open("wordlist.txt", "w")
 
 file.write(content.encode("utf-8"))
